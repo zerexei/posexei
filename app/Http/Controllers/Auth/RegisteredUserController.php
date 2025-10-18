713f22abb010392d5fb1bfb89750bec66196f927
Angelo Arcillas <angeloarcillas64@gmail.com>
2025-10-18 15:42:33 +0800

    Add Organization Model

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -42,6 +42,8 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        // create organization
+
         event(new Registered($user));
 
         Auth::login($user);
