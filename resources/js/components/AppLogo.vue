bcd490dc01b65a16de20d4ae4f8ef1264e06799b
Angelo Arcillas <angeloarcillas64@gmail.com>
2026-03-01 22:32:01 +0800

    feat: replace logo image with SVG for improved scalability and styling

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -3,8 +3,8 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
 </script>
 
 <template>
-    <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-        <AppLogoIcon class="size-4 fill-current text-white dark:text-black" />
+    <div class="flex aspect-square size-8 items-center justify-center rounded-xl p-2">
+        <AppLogoIcon class="size-4 text-white" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
         <span class="mb-0.5 truncate leading-tight font-semibold">Posexei</span>
--- resources/js/components/AppLogoIcon.vue
+++ resources/js/components/AppLogoIcon.vue
@@ -13,5 +13,19 @@ defineProps<Props>();
 </script>
 
 <template>
-    <img src="https://avatars.githubusercontent.com/u/69749146" alt="logo" class="size-full rounded-md object-contain" />
+    <svg 
+        viewBox="0 0 24 24" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg" 
+        :class="className"
+    >
+        <path 
+            d="M13 2L3 14H12L11 22L21 10H12L13 2Z" 
+            fill="currentColor" 
+            stroke="currentColor" 
+            stroke-width="2" 
+            stroke-linecap="round" 
+            stroke-linejoin="round"
+        />
+    </svg>
 </template>
