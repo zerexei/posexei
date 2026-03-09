16ebfdaa86d508d4f0426791fe39019120c6efda
Angelo Arcillas <angeloarcillas64@gmail.com>
2026-03-09 12:57:59 +0800

    Remove server config in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,33 +2,26 @@ import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
-
-    return {
-        server: {
-            allowedHosts: [env.NGROK_DOMAIN],
-        },
-        plugins: [
-            laravel({
-                input: ['resources/js/app.ts'],
-                ssr: 'resources/js/ssr.ts',
-                refresh: true,
-            }),
-            tailwindcss(),
-            wayfinder({
-                formVariants: true,
-            }),
-            vue({
-                template: {
-                    transformAssetUrls: {
-                        base: null,
-                        includeAbsolute: false,
-                    },
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/app.ts'],
+            ssr: 'resources/js/ssr.ts',
+            refresh: true,
+        }),
+        tailwindcss(),
+        wayfinder({
+            formVariants: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
                 },
-            }),
-        ],
-    };
+            },
+        }),
+    ],
 });
