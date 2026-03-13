4b861a4f97780ec261b1c16e5fc6cf80d5b50966
Angelo Arcillas <angeloarcillas64@gmail.com>
2026-03-13 19:43:34 +0800

    Add Organization->owner return type

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Organization extends Model
 {
@@ -11,7 +12,7 @@ class Organization extends Model
         'name',
     ];
 
-    public function owner()
+    public function owner(): BelongsTo
     {
         return $this->belongsTo(User::class, 'owner_id');
     }
