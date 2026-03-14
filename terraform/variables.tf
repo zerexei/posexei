932b090ddb7843b8132b198cca33eab9c6f764b5
Angelo Arcillas <angeloarcillas64@gmail.com>
2026-03-14 16:43:48 +0800

    style: update Terraform variable type definitions from string literals to direct type names.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,24 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
-  type        = "string"
+  type        = string
   default     = "us-east-1"
 }
 
 variable "project_name" {
   description = "Name of the project"
-  type        = "string"
+  type        = string
   default     = "posexei"
 }
 
 variable "environment" {
   description = "Deployment environment (e.g. prod, staging)"
-  type        = "string"
+  type        = string
   default     = "prod"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
-  type        = "string"
+  type        = string
   default     = "10.0.0.0/16"
 }
 
