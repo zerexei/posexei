3202fb79371ef2e8690818f608a7ee59f237e963
Angelo Arcillas <angeloarcillas64@gmail.com>
2026-03-14 13:28:46 +0800

    Add PROCESSING SocialChannelStatusEnum

--- app/Enums/Social/SocialChannelStatus.php
+++ app/Enums/Social/SocialChannelStatus.php
@@ -5,6 +5,7 @@ namespace App\Enums\Social;
 enum SocialChannelStatus: string
 {
     case CONNECTED = 'connected';
+    case PROCESSING = 'processing';
     case DISCONNECTED = 'disconnected';
     case EXPIRED = 'expired';
 }
