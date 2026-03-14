5074f51abf04eac64d1a397b4888984433620ad3
Angelo Arcillas <angeloarcillas64@gmail.com>
2026-03-14 16:04:41 +0800

    chore(core): add opentelemetry dependencies and register providers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->prepend(\App\Http\Middleware\TraceRequest::class);
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
