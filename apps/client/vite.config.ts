09b0bba38c38c2fe11a96a7e02a5514aeab86ab2
Angelo Arcillas <angeloarcillas64@gmail.com>
2026-04-17 21:50:30 +0800

    Uncomment vite server for docker-compose setup

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -5,13 +5,13 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-    // server: {
-    //     host: true,
-    //     port: 3000,
-    //     watch: {
-    //         usePolling: true,
-    //     },
-    // },
+    server: {
+        host: true,
+        port: 3000,
+        watch: {
+            usePolling: true,
+        },
+    },
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
